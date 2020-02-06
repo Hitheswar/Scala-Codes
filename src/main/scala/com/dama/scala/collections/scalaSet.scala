9e3fb80718af46c53589e875d8922b1574ba7028
hitheswar <dama.hitheswar@aline-consulting.com>
2020-02-06 13:29:54 +0530

    maps & sets

---
+++ src/main/scala/com/dama/scala/collections/scalaSet.scala
@@ -0,0 +1,29 @@
+package com.dama.scala.collections
+
+import scala.collection.SortedSet
+
+/*
+It is used to store unique elements in the set. It does not maintain any order for storing elements. You can apply various operations on them.
+It is defined in the Scala.collection.immutable package
+ */
+object scalaSet {
+  def main(args: Array[String]): Unit = {
+    val set1 = Set()                            // An empty set
+    val games = Set("Cricket","Football","Hocky","Golf")    // Creating a set with elements
+    println(set1)
+    println(games)
+
+    println(games.head, games.tail,games.isEmpty)
+
+    for(game <- games){
+      println("game "+game)
+    }
+
+    games.foreach((game:String)=>println("game "+game))  //using foreach loop
+
+    var numbers: SortedSet[Int] = SortedSet(5,8,1,2,9,6,4,7,2)
+    numbers.foreach((element:Int)=> print(element+", ")) //returns a Set after sorting elements
+
+
+  }
+}
--- src/main/scala/com/dama/scala/concepts/level1/scalaMap.scala
+++ src/main/scala/com/dama/scala/concepts/level1/scalaMap.scala
@@ -1,11 +1,35 @@
 package com.dama.scala.concepts.level1
 /*
  Map is a collection of key/value pairs. Scala retrieves a Value based on its Key.
+
+ A Map in Scala is a collection of key-value pairs, and is also called a hash table. We can use a key to access a value.
+ These keys are unique; however, the values may be common. The default Scala Map is immutable. To use a mutable Map,
+ we use the scala.collection.mutable.Map class.
+ To use both in the same place, refer to the immutable Map as Map, and to the mutable Map as mutable.Map.
  */
 object scalaMap {
   def main(args: Array[String]): Unit = {
 
-    val colors = Map("red" -> "#FF0000", "azure" -> "#F0FFFF")
+    val emptyMap:Map[String,Int] = Map()        //declaring empty map
+    println(emptyMap.isEmpty)       //true
 
+
+    var colors = Map("red" -> "#FF0000", "azure" -> "#F0FFFF")
+    colors +=("blue"->"#hjil")
+
+    println("keys "+colors.keys)      //Set(red, azure, blue)
+
+    println("colors(\"azure\") "+colors("azure"))   //#F0FFFF
+
+    println("values "+colors.values)    //MapLike.DefaultValuesIterable(#FF0000, #F0FFFF, #hjil)
+
+    var colors2 = Map("yellow"->"#jklo","black"->9)
+
+    var concat = colors++colors2      //concatination
+    //concat.foreach(println)
+
+    concat.foreach(x=>
+    println(x._1,x._2)
+    )
   }
 }
